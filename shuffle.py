a3ed84143d5f2e3a773988c516dcada4db2b5ddd
rainbowlemons <92555787+rainbowlemons@users.noreply.github.com>
2021-12-10 15:51:26 +0000

    Update shuffle.py

--- shuffle.py
+++ shuffle.py
@@ -1,5 +1,6 @@
+#!/usr/bin/python
 import random
 
-lines = open("words_shuffled.txt").readlines()
+lines = open("words_alpha.txt").readlines()
 random.shuffle(lines)
 open("words_shuffled.txt", "w").writelines(lines)
